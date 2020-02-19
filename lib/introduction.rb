e4886523c9e6970b51174b7c0f9a6fd1969260aa
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-19 18:20:44 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,7 +7,9 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
+def introdction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
